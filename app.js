740af80f9c270bfd2314d2672797941df02813fb
AmrAkmal <amrakmal97@gmail.com>
2021-01-06 19:32:35 +0200

    serve static files

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-app.use(express.json())
-
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
     req.requestTime = new Date();
     const delay = Math.floor(Math.random() * 10) * 1000;
