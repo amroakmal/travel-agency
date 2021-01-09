af34bab944c9f591a072e49762c4a878a90816e9
AmrAkmal <amrakmal97@gmail.com>
2021-01-09 10:32:53 +0200

    Connect to Database using mongoose

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
+
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+mongoose.connect(DB, {
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useNewUrlParser: true
+})
+    .then(con => {
+        console.log('------');
+        console.log(con.connections);
+        console.log('------');
+    })
 
 const app = require('./app');
 
