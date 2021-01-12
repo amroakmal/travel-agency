e93969b9d69824978e415ccf14b46b3a12a361c0
AmrAkmal <amrakmal97@gmail.com>
2021-01-12 20:25:52 +0200

    Create global error handling middleware

--- app.js
+++ app.js
@@ -19,11 +19,30 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-app.use((req, res) => {
-    return res.status(404).json({
-        status: 'failed',
-        message: 'Page not found'
-    })
+// app.use((req, res) => {
+//     return res.status(404).json({
+//         status: 'failed',
+//         message: 'Page not found'
+//     })
+// });
+
+app.all('*', (req, res, next) => {
+    const err = new Error(`Can't find the ${req.originalUrl} on the server`);
+    err.statusCode = 404;
+    err.status = 'fail';
+    
+    next(err);
 });
+
+//Create Globa; error handling middleware
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    })
+})
 
 module.exports = app;
