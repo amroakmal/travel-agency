c97415a268a493322382e287baba1e347711f96f
AmrAkmal <amrakmal97@gmail.com>
2021-01-16 15:11:42 +0200

    Only take needed information from req.body when crating new useer

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,13 @@ const catchAsync = require('../utils/catchAsync');
 const UserModel = require('../models/userModel');
 
 exports.signup = catchAsync(async(req, res, next) => {
-    const newUser = await UserModel.create(req.body);
+    const newUser = await UserModel.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm,
+        photo: req.body.photo
+    });
     
     res.status(201).json({
         status: 'success',
