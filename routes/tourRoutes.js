f0c84c0d88aaed8c22751b2a131db962132cd92c
AmrAkmal <amrakmal97@gmail.com>
2021-01-23 16:50:57 +0200

    some code refactoring

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -8,7 +8,7 @@ router.route('/top-5-cheap')
     .get(
         tourController.aliasTopTours,
         tourController.getAllTours
-    );
+    ); 
 
 router.route('/tour-stats')
     .get(tourController.getTourStats);
